6c749921dd446ad1bc64ca71ee7ce337268c3d49
Luis <luiseddie.kr@gmail.com>
2016-06-15 22:14:19 +0100

    Added snake draft order

--- app/controllers/draft/DraftManagerActor.java
+++ app/controllers/draft/DraftManagerActor.java
@@ -40,7 +40,9 @@ public class DraftManagerActor extends UntypedActor {
     public static String getCurrentUser(int turn, ArrayList<String> users) {
         if (turn<0 || users.size()==0 || turn>=users.size()*PICKS_PER_PLAYER) return null;
 
-        return users.get(turn % users.size());
+        int i = turn % 2*users.size();
+        if (i >= users.size()) i = 2*users.size()-1 - i;
+        return users.get(i);
     }
 
     public DraftManagerActor() {
